bc8233715d2e4ac27b1fd5f8d1f59bc10e3e3459
Hhpon <624791164@qq.com>
2018-11-30 23:37:59 +0800

    add mint-ui

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import App from './App'
 import router from './router'
-import BScroll from 'better-scroll'
 import '../static/css/reset.css'
 
 Vue.config.productionTip = false
+Vue.use(MintUI)
 
 /* eslint-disable no-new */
 new Vue({
