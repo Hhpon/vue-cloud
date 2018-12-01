854d05a5623c74e10e0b7c8457f887647a2a7f3c
Hhpon <624791164@qq.com>
2018-12-01 23:48:33 +0800

    探究轮播图2

--- src/components/Scroll/Scroll.vue
+++ src/components/Scroll/Scroll.vue
@@ -1,58 +1,75 @@
 <template>
   <div>
     <div class="carousel-wrap">
-      <transition-group tag="ul" name="list" class="scroll-ul">
+      <transition-group tag="ul" name="list" class="scroll-ul" :style="{height: imgHeight+'px'}">
         <li v-for="(img,index) in imgs" v-show="index === mark" :key="index">
           <a :href="img.url">
-            <img :src="img.imageUrl" @load="loadHandle" :height="imgHeight+'px'" ref="img">
+            <img :src="img.imageUrl" ref="img">
           </a>
         </li>
       </transition-group>
     </div>
-    <button @click="getHeight">1241241243</button>
   </div>
 </template>
 <script>
 import axios from "axios";
-import { setInterval } from "timers";
 export default {
   data() {
     return {
-      mark: 0,
+      mark: 1,
       imgs: [],
-      name: "",
-      imgHeight: 0
+      imgHeight: 0,
+      timer: ""
     };
   },
-  created() {},
+  beforeCreate() {
+    // this.getImgs();
+    axios.get("http://localhost:3000/banner").then(res => {
+        this.imgs = res.data.banners;
+        let realWidth =
+          window.innerWidth ||
+          document.documentElement.clientWidth ||
+          document.body.clientWidth;
+        let img = new Image();
+        img.src = res.data.banners[0].imageUrl;
+        let that = this;
+        img.onload = function() {
+          that.imgHeight = (img.naturalHeight * realWidth) / img.naturalWidth;
+        };
+      });
+  },
+  created() {
+    // this.getImgs();
+  },
   mounted() {
-    this.getImgs();
-    this.name = "image";
-    this.play();
+    this.$nextTick(() => {
+      this.timer = setInterval(this.autoplay, 3000);
+    });
   },
   methods: {
     getImgs() {
       axios.get("http://localhost:3000/banner").then(res => {
         this.imgs = res.data.banners;
-        console.log(res.data.banners);
+        let realWidth =
+          window.innerWidth ||
+          document.documentElement.clientWidth ||
+          document.body.clientWidth;
+        let img = new Image();
+        img.src = res.data.banners[0].imageUrl;
+        let that = this;
+        img.onload = function() {
+          that.imgHeight = (img.naturalHeight * realWidth) / img.naturalWidth;
+        };
       });
     },
     autoplay() {
       this.mark++;
-      var imgsLength = this.imgs.length;
-      if (this.mark === imgsLength) {
+      if (this.mark === this.imgs.length) {
         this.mark = 0;
       }
     },
     play() {
       setInterval(this.autoplay, 3000);
-    },
-    getHeight() {
-      console.log(this.$refs.img[0].naturalHeight);
-      this.imgHeight = this.$refs.img[0].naturalHeight;
-    },
-    loadHandle(){
-      console.log('加载完毕');
     }
   }
 };
@@ -77,11 +94,11 @@ export default {
 }
 
 .list-enter-to {
-  transition: all 3s ease;
+  transition: all 0.5s ease;
   transform: translateX(0);
 }
 .list-leave-active {
-  transition: all 3s ease;
+  transition: all 0.5s ease;
   transform: translateX(-100%);
 }
 .list-enter {
